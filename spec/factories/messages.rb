aac15500c5b250408eece439fb85f38e060106fb
mamiko3 <mmurakami722@gmail.com>
2020-02-14 19:06:57 +0900

    summary(required)

---
+++ spec/factories/messages.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  
+  factory :message do
+    body {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/test_image.png")}
+    user
+    group
+  end
+end
