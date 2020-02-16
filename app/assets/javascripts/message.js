27e000c3e60f612d71e2df7721ffc638ac88c9ff
mamiko3 <mmurakami722@gmail.com>
2020-02-16 20:28:41 +0900

    Summary(required)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,45 @@
 $(function(){
-
-  $('#new_message').on('submit', function(e){
+    function buildHTML(message) {
+      if (message.image) {
+        var html = 
+          `<div class="message" data-message-id=${message.id}>
+            <div class="message__list">
+              <div class="message__list--name>
+                ${message.user_name}
+              </div>
+              <div class="message__list--timestamp">
+                ${message.created_at}
+              </div>
+              <div class="message__text">
+                <p>
+                </p>
+                ${message.body}
+              </div>
+              <img src=${message__text__image}
+            </div>
+          </div>`
+        return html;
+      } else {
+        var html =
+          `<div class="message" data-message-id=${message.id}>
+            <div class="message__list">
+              <div class="message__list--name">
+                ${message.user_name}
+              </div>
+              <div class="message__list--timestamp">
+                ${message.created_at}
+              </div>
+              <div class="message__text">
+                <p>
+                </p>
+                ${message.body}
+              </div>
+            </div>
+          </div>`
+          return html;
+        };
+        }
+$('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -12,5 +51,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message').append(html);
+      $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0] .scrollHeight});
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   });
 });
